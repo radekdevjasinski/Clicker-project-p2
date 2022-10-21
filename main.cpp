9f855a8f1b21102ba3ed9ceef38e033f9ba97b0a
AnchorDev <01.jakub.kotwica@gmail.com>
2022-10-21 17:09:41 +0200

    gotowa kasa z przecinkami

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
+class Money
+{
+public:
+    int cash, count = 0, d = 1;
+    string WriteMoney() {
+        int num = cash;
+        string str = "";
+        while (cash) {
+            cash /= 10;
+            count++;
+            if (cash) d *= 10;
+        }
+        cash = num;
+
+        while (num) {
+            if (count-- % 3 == 0 and cash != num) str += ",";
+
+            str += to_string(num/d);
+            num %= d;
+            d /= 10;
+        }
+        return str;
+    }
+}money;
 
 int main() {
-
+    money.cash = 1235678;
+    cout << money.WriteMoney();
     return 0;
+
 }
