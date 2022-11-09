91ff51fd881ec0dd1c9da0058fce0994e881502b
radek.jasiu.jasinski@gmail.com <radek.jasiu.jasinski@gmail.com>
2022-11-09 17:41:13 +0100

    timeworking

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ public:
     }
     void MoneyAdd(int cash_) 
     {
-        cash = cash_;
+        cash += cash_;
     }
 }money;
 class Job {
@@ -61,6 +61,7 @@ public:
         reward = reward_;
         secondsToGo = secondsToGo_;
         timeToShow = 0;
+        StartWorking();
     }
     void StartWorking() 
     {
@@ -70,13 +71,13 @@ public:
     {
         if (!isLocked)
         {
-            cout << startTime;
-            int CirclesDoneInt = (time(NULL) - startTime) / secondsToGo;
-            float CirclesDoneFloat = (time(NULL) - startTime) / secondsToGo - CirclesDoneInt;
+            int currentTime = time(NULL);
+            int CirclesDoneInt = (currentTime - startTime) / secondsToGo;
+            float CirclesDoneFloat = (currentTime - startTime) % secondsToGo;
             if (CirclesDoneInt >= 1)
             {
                 money.MoneyAdd(reward * CirclesDoneInt);
-                startTime += CirclesDoneInt * secondsToGo;
+                startTime = currentTime;
             }
             timeToShow = (float)(CirclesDoneFloat / secondsToGo);
         }
@@ -117,7 +118,6 @@ public:
                 cout << "PRICE: " << it->price << endl;
                 cout << "TIME: " << it->timeToShow << endl;
                 cout << "LEVEL: " << it->level << " / " << it->maxLevel << "\n\n";
-                it->StartWorking();
             }
             else
             {
